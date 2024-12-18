fdf2e773ed5139a0acc0e1adb114b8bfadbe3186
arthur <arthurazevedods@gmail.com>
2024-12-18 18:47:50 -0300

    posts

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -1,11 +1,21 @@
 import Banner from "../../components/Banner/Banner";
+import Post from "../../components/Post/Post";
+import styles from './Inicio.module.css';
+
+import posts from '../../json/posts.json';
 
 export default function Inicio() {
     return (
         <>
             <main>
                 <Banner></Banner>
-                <h1>Olá, mundo</h1>
+                <ul className={styles.posts}>
+                    {posts.map((post)=>(
+                        <li key={post.id}>
+                            <Post post={post}/>
+                        </li>
+                    ))}
+                </ul>
             </main>
 
         </>
